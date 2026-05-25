32170ef8d1ff80e7e02352dfbd2021cf9106fa20
iki1uc <entertrainer@web.de>
2026-05-25 18:28:06 +0200

    Update out0.js

--- out0.js
+++ out0.js
@@ -1,1 +1,18 @@
+// out0.js – Wissenschaftlicher Output für Cube‑0
+
+function out0_write(ist, soll, ableitung) {
+  document.getElementById("out").innerHTML = `
+    <div class="out-title">Wissenschaftlicher Output – Cube‑0</div>
+    <div class="out-ist"><b>IST:</b> ${ist}</div>
+    <div class="out-soll"><b>SOLL:</b> ${soll}</div>
+    <div class="out-ableitung"><b>Ableitung:</b> ${ableitung}</div>
+  `;
+}
+
+// Beispielstart
+out0_write(
+  "System bereit",
+  "System synchron",
+  "Cube‑0 dient als Master‑Pipeline und versorgt alle Cubes."
+);
 
